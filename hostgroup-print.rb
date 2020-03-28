eadb9eaea77f555f0189b68ae735541ceea7a49e
Massimiliano Adamo <maxadamo@gmail.com>
2020-03-28 09:28:36 +0100

    amend hostgroup

--- hostgroup-print.rb
+++ hostgroup-print.rb
@@ -4,17 +4,10 @@ host = ARGV[0]
 hostgrp = host.downcase.gsub(%r{[0-9]}, '').gsub(%r{(^lon|^ch)-}, '').gsub(%r{(^dev|^test|^uat|^prod|^prd)-}, '')
 hostgrp = if hostgrp.include? 'psma-gn-'
             'geant-psma'
-          elsif [
-            'psmp-gn-bw', 'ps-test', 'psmp-lhc-mgmt', 'psmp-gn-mgmt-'
-          ].any? { |w| hostgrp.include?(w) }
+          elsif ['psmp-gn-bw', 'ps-test', 'psmp-lhc-mgmt', 'psmp-gn-mgmt-'].include?(hostgrp)
             'geant-psmp'
           elsif ['rhps', 'rhpsc'].include?(hostgrp)
             'perfsonarkit'
-          # these nodes seem they are gone
-          #elsif hostgrp.include? 'ch-dev-'
-          #  'ch-dev'
-          elsif hostgrp.include? 'jenkins'
-            'jenkins'
           else
             hostgrp
           end
