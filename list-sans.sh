1afe66afa1ad11a02a0e253a3b5d748ac1403764
Massimiliano Adamo <maxadamo@gmail.com>
2020-12-14 16:18:55 +0100

    minor

--- list-sans.sh
+++ list-sans.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ]; then
     echo "You need to supply the domain name to check"
+    echo "and optionally the second argument is the port number"
+    exit
 fi
 
-openssl s_client -connect ${1}:443 </dev/null 2>/dev/null |
+if [ "$#" -eq 1 ]; then
+    PORT=$2
+else
+    PORT="443"
+fi
+
+echo "connecting to $1 on port $2 ..."
+
+openssl s_client -connect ${1}:${PORT} </dev/null 2>/dev/null |
     openssl x509 -noout -text |
     awk '/DNS:/{
-        gsub(/, DNS:/, "\"\n - \"");
-        sub(/^.*DNS:/, " - \"");
+        gsub(/, DNS:/, "\"\n  - \"");
+        sub(/^.*DNS:/, "  - \"");
         printf $0"\"\n"
     }'
+
