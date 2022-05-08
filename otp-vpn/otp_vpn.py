b5abdc73f15203bc3f20fb290da5e98be65399f2
Massimiliano Adamo <maxadamo@gmail.com>
2022-05-08 12:46:10 +0200

    use only ipv4 addresses

--- otp-vpn/otp_vpn.py
+++ otp-vpn/otp_vpn.py
@@ -112,8 +112,8 @@ dev tun
 #log {MY_USER_DIR}/jump.log
 remote 83.97.92.126 1194
 remote 83.97.92.163 1194
-remote 2001:798:3::96 1194
-remote 2001:798:3::bb 1194
+#remote 2001:798:3::96 1194
+#remote 2001:798:3::bb 1194
 connect-timeout 3
 connect-retry 2
 connect-retry-max 2
