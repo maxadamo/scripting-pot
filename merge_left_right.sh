5fd4126313c5dcd2a421b2f40a9efd81bdc449e9
Massimiliano Adamo <maxadamo@gmail.com>
2025-11-03 11:49:36 +0100

    allow using either prod or production

--- merge_left_right.sh
+++ merge_left_right.sh
@@ -7,37 +7,43 @@ fi
 
 git-up
 
-echo -e "\nYou are merging the repository $(basename $(pwd))\n"
+echo -e "\nYou are merging the repository $(basename "$(pwd)")\n"
 read -p "Are you sure? [Yy] " -n 1 -r
 echo
+
+if git branch -a | grep "remotes/origin/production" &>/dev/null; then
+    prod_branch='production'
+else
+    prod_branch='prod'
+fi
 
 if [[ $REPLY =~ ^[yY]$ ]]; then
 
     MYBRANCH=$(git rev-parse --abbrev-ref HEAD)
     SEPARATOR="\n============================"
 
-    if [ $MYBRANCH == 'test' ]; then
+    if [ "$MYBRANCH" == 'test' ]; then
         git merge uat; git push
-        git merge production; git push
+        git merge "$prod_branch"; git push
         git checkout uat
         git merge test
         git push
-        git checkout production
+        git checkout "$prod_branch"
         git merge test
         git push
     fi
 
-    if [ $MYBRANCH == 'uat' ]; then
+    if [ "$MYBRANCH" == 'uat' ]; then
         # try to merge back first
-        git merge production; git push
-        # checkout production
-        git checkout production
+        git merge "$prod_branch"; git push
+        # checkout "$prod_branch"
+        git checkout "$prod_branch"
         git merge uat
         git push
     fi
 
     echo -e "${SEPARATOR}\nswitching back to $MYBRANCH"
-    git checkout $MYBRANCH
+    git checkout "$MYBRANCH"
     echo -e "${SEPARATOR}\n"
 
 fi
